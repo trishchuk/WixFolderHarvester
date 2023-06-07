bc7d7082ba43d8bf50bda5d2f316120e3dc705b7
Ruslan Hadyniak <rushad@auditdata.com>
2023-06-07 12:34:57 +0300

    added support for .gitignore patterns using Ignore NuGet

---
+++ WixFolderHarvester/Helpers/WixIgnoreHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace WixFolderHarvester.Helpers
+{
+    internal static class WixIgnoreHelper
+    {
+        public static string[] ReadIgnorePatterns(string ignoreFilePath)
+        {
+            var ignorePatterns = File.ReadAllLines(ignoreFilePath)
+                .Select(line => line.Trim())
+                .Where(x => !string.IsNullOrWhiteSpace(x)) // Remove blank lines
+                .Where(x => !x.StartsWith("#"))  // Remove comments
+                .ToArray();
+            return ignorePatterns;
+        }
+    }
+}
